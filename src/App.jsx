65327c3c219020d44d18555e41e88db5b08e813c
MariangelRondan <rondanmariangel@gmail.com>
2023-11-21 20:32:33 -0300

    error page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./components/LandingPage/LandingPage"
 import FormPage from "./components/FormPage/FormPage"
 import Home from "./components/Home/Home"
 import Detail from './components/Detail/Detail';
+import Error from "./components/Error/Error";
 
 
 function App() { 
@@ -13,7 +14,9 @@ function App() {
         <Route exact path="/" element={<LandingPage/>} />
         <Route  path='/home' element={<Home/>}/>
         <Route  path='/detail/:id' element={<Detail/>}/>
+        
         <Route  path="/create" element={<FormPage/>}/>
+        <Route path="*" element={<Error />} />
 </Routes>
     </div>
   );
